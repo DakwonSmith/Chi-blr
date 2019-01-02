0edf6fd26ef9c94cf86af9b18dc2912b0e27a597
DakwonSmith <dsmith@nyc.yearup.org>
2019-01-02 09:45:08 -0500

    Preparing to deploy Heroku

--- server.rb
+++ server.rb
@@ -1,4 +1,33 @@
 require 'sinatra'
+require "sinatra/reloader"
+
+# Run this script with `bundle exec ruby app.rb`
+require 'sqlite3'
+require 'active_record'
+
+#require classes
+require './models/user.rb'
+require './models/email.rb'
+require './models/post.rb'
+
+# Use `binding.pry` anywhere in this script for easy debugging
+require 'pry'
+require 'csv'
+
+# Connect to a sqlite3 database
+# If you feel like you need to reset it, simply delete the file sqlite makes
+if ENV['DATABASE_URL']
+  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])  
+else
+  ActiveRecord::Base.establish_connection(
+    adapter: 'sqlite3',
+    database: 'db/development.db'
+  )
+end
+
+
+register Sinatra::Reloader
+enable :sessions
 
 get '/' do
 	erb :index
